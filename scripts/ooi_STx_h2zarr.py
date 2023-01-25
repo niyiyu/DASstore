677391cddee6f32fdb7837b8b3bc21b419c14c6b
John Ragland <jhrag@uw.edu>
2023-01-25 21:40:52 +0000

    changed ooi_STx_h2zarr.py scripts to use module

--- scripts/ooi_STx_h2zarr.py
+++ scripts/ooi_STx_h2zarr.py
@@ -7,40 +7,13 @@ import numpy as np
 import os
 import h5py
 from tqdm import tqdm
+from DASStore.modules import convert
 
-file_dir = '/das_data2/h5files/'
-file_list = os.listdir(file_dir)
+# Download all files from RCA Server
+os.system('bash batch_download.sh')
 
-# remove .aria2 files (only for debugging during transfer)
-valid_idx = []
-for k, file in enumerate(file_list):
-    if file[-2] != 'a':
-        valid_idx.append(k)
-        
-file_list = [file_list[i] for i in valid_idx]
-file_list.sort()
+# load list of files from disk
+h5dir = '/das_data/h5files/'
+zarrdirr = '/das_data/zarr/ooi_South_Tx.zarr'
 
-files = [file_dir+file for file in file_list]
-
-first_loop=True
-
-for file in tqdm(files):
-    hf = h5py.File(file)
-    
-    ds = xr.Dataset({
-        'RawData':((['distance', 'time'], hf['Acquisition']['Raw[0]']['RawData'][:])),
-        'RawDataTime':('time', hf['Acquisition']['Raw[0]']['RawDataTime'][:]),
-        'GpBits':('time', hf['Acquisition']['Raw[0]']['Custom']['GpBits']),
-        'GpsStatus':('time', hf['Acquisition']['Raw[0]']['Custom']['GpsStatus']),
-        'PpsOffset':('time', hf['Acquisition']['Raw[0]']['Custom']['PpsOffset']),
-        'SampleCount':('time', hf['Acquisition']['Raw[0]']['Custom']['SampleCount'])}
-    )
-    
-    ds = ds.chunk({'time':3000, 'distance':3000})
-    
-    # create new zarr store if beginning of loop otherwize, append in time dimension
-    if first_loop:
-        first_loop = False
-        ds.to_zarr(f'/das_data2/SouthCable_Tx_{files[0][-21:-4]}{files[-1][-21:-4]}', mode='w-')
-    else:
-        ds.to_zarr(f'/das_data2/SouthCable_Tx_{files[0][-21:-4]}{files[-1][-21:-4]}', append_dim='time')
+convert.h52zarr_xarray(h5dir, zarrdirr)
