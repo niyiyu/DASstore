6b68d60ec3f92ef0cd44d3caa3a9b9f5a6885088
Yiyu Ni <niyiyu@uw.edu>
2023-03-31 01:17:13 -0700

    add setup and update requirement

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    reqs = f.read().split("\n")
+
+setup(
+    version="0.0.1",
+    name="dasstore",
+    packages=find_packages(),
+    include_package_data=True,
+    python_requires=">=3.7",
+    install_requires=reqs,
+    author="Yiyu Ni",
+    author_email="niyiyu@uw.edu",
+    description="An object storage system for Distributed Acoustic Sensing data",
+    license="GPL-3.0 license",
+    url="https://github.com/niyiyu/dasstore",
+)
